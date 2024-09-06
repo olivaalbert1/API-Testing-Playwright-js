dc82b5423742586625c62a525f84686b0b76c296
Albert Oliva <oliva.albert+1@gmail.com>
2024-09-06 20:58:56 +0200

    add POST test

--- tests/api-tests.spec.js
+++ tests/api-tests.spec.js
@@ -1,12 +1,28 @@
 import { test, expect } from "@playwright/test"
 
-test('API Get request', async({request}) => {
+test('API GET request', async({request}) => {
     const response = await request.get('https://reqres.in/api/users/2')
 
     expect(response.status()).toBe(200)
     
     const responseText = await response.text()
     expect(responseText).toContain('Janet')
+    
+    console.log(await response.json())
+})
+
+test('API POST request', async({request}) => {
+    const response = await request.post('https://reqres.in/api/users',{
+        data: {
+            "name": "Albert",
+            "job": "QA"
+        }
+    })
+
+    expect(response.status()).toBe(201)
+    
+    const responseText = await response.text()
+    expect(responseText).toContain('Albert')
     
     console.log(await response.json())
 })
